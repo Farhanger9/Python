01aa764967523a25fc7cae653888d01188a43fea
Farhang Er <frankie.er9@gmail.com>
2020-11-17 06:25:49 +0000

    Day 3 and 4

--- main.py
+++ main.py
@@ -14,14 +14,48 @@
 
 #Final Project
 
-print("Welcome to the tip calculator!")
-bill = float(input("What was the total bill?: $"))
-tip = int(input("How much tip would you like to give? 10, 12 or 15? "))
-people = int(input("How many people to split the bill? "))
-bill_with_tip = tip / 100 * bill + bill
-bill_person = bill_with_tip / people
-final_amount = round(bill_person,2)
-print(f"each person should pay {final_amount} dollar")
+# print("Welcome to the tip calculator!")
+# bill = float(input("What was the total bill?: $"))
+# tip = int(input("How much tip would you like to give? 10, 12 or 15? "))
+# people = int(input("How many people to split the bill? "))
+# bill_with_tip = tip / 100 * bill + bill
+# bill_person = bill_with_tip / people
+# final_amount = round(bill_person,2)
+# print(f"each person should pay {final_amount} dollar")
+
+
+#Day 3 and 4
+
+# print("Welcome to Python Pizza Deliveries!")
+# size = input("What size pizza do you want? S, M, or L ")
+# add_pepperoni = input("Do you want pepperoni? Y or N ")
+# extra_cheese = input("Do you want extra cheese? Y or N ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+# bill = 0
+
+# if size == "S":
+#   bill += 15
+# elif size == "M":
+#   bill += 20
+# else:
+#   bill += 25
+
+# if add_pepperoni == "Y":
+#   if size == "S":
+#     bill += 2
+#   else:
+#     bill += 3
+    
+# if extra_cheese == "Y":
+#   bill += 1
+  
+# # print(f"Your final bill is: ${bill}.")
+
+
+
 
 
 
